927771f32d0b476365c4bc97f9107195547f2621
Saraansh Wadkar <saraanshwadkar@gmail.com>
2026-05-12 16:34:51 -0400

    add markdown preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = {
+      "MarkdownPreview",
+      "MarkdownPreviewStop",
+      "MarkdownPreviewToggle",
+    },
+    ft = { "markdown" },
+    build = "cd app && ./install.sh",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+  },
+}
