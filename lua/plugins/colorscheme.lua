b0eb94ee64c1dd581584c9cd5bb4eb240b9e4086
Saraansh Wadkar <saraanshwadkar@gmail.com>
2026-05-17 10:07:31 -0400

    make more translucent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,18 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "night",
+      transparent = false,
+      styles = {
+        sidebars = "dark",
+        floats = "dark",
+      },
+      on_colors = function(colors)
+        -- Slightly denser backgrounds so the editor reads better over a translucent terminal.
+        colors.bg = "#161821"
+        colors.bg_dark = "#14161d"
+        colors.bg_float = "#161821"
+        colors.bg_sidebar = "#14161d"
+      end,
     },
   },
 
